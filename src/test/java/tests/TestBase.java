c90f9530b4de66a759f9623983de209afb61398d
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-08-24 19:29:09 +0300

    changed TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,14 +20,14 @@ public class TestBase {
     void setUp() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://sport-marafon.ru/");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "138.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("remoteUrl");
+        //Configuration.remote = System.getProperty("remoteUrl");
         //Configuration.pageLoadStrategy = "eager";
         Configuration.pageLoadTimeout = 60000;
         //Configuration.holdBrowserOpen = true;
 
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
